c3e755b6ffdc8458a8c548307428a57b71ed617d
LeonardoLDRE <1559747@senati.pe>
2025-02-27 17:31:24 -0500

    git commit -m "np multiplicacion de dos listas"

---
+++ numpy _ejercicio/multiplicacion_de_dos_listas.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array(list(map(int, 
+input("ingresar los 5 numeros:").split())))
+b = np.array(list(map(int, 
+input("ingresar los 5 numeros:").split())))
+print("el resultado es:", a * b)
